cb0cefda0d302c9152af13859455454f0cfacca1
GustavoMacias <gustavo.maci.1234@gmail.com>
2022-07-05 21:27:59 -0500

    Ejercicio Listado de personas

---
+++ JavaScriptHtmlCss/Leccion03_ListadoPersonas/js/app.js
@@ -0,0 +1,28 @@
+const personas = [
+new Persona("Juan","Perez"),
+new Persona("Pedro","Gomez")
+];
+
+function mostrarPersonas(){
+    console.log("Mostrando personas");
+    let texto = "";
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms["forma"];
+    const nombre = forma["nombre"];
+    const apellido = forma["apellido"];
+    if(nombre.value != "" && apellido.value != ""){
+    const persona = new Persona(nombre.value,apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+    }else{
+        console.log("no hay informacion que agregar")
+    }
+}
